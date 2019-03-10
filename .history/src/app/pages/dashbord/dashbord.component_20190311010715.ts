b7b9affae387ae6a5d91916aab9e5cd77486938a
berry <berry@berrydeMacBook-Pro.local>
2019-03-11 01:12:02 +0700

    smart-table variavles name update

---
+++ .history/src/app/pages/dashbord/dashbord.component_20190311010715.ts
@@ -0,0 +1,114 @@
+import { Component } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
+import { takeWhile } from 'rxjs/operators' ;
+import { CardService, Summary } from './card.service';
+import { formatDate } from '@angular/common';
+
+
+
+interface CardSettings {
+  title: string;
+  iconClass: string;
+  type: string;
+  info: string | number;
+}
+
+
+@Component({
+  selector: 'ngx-dashbord',
+  templateUrl: './dashbord.component.html',
+})
+export class ECommerceComponent {
+
+  myDate = formatDate(new Date(), 'yyyy-MM-dd', 'en');
+  cards: Summary[] = [];
+
+  private alive = true;
+// ng 指令 carddgroup;
+  solarValue: number;
+  dateCard: CardSettings = {
+    title: '日期',
+    info: '',
+    iconClass: 'nb-compose',
+    type: 'primary',
+  };
+  totalUserCard: CardSettings = {
+    title: '统计人数',
+    info: '',
+    iconClass: 'nb-roller-shades',
+    type: 'success',
+  };
+  checkedCountCard: CardSettings = {
+    title: '打卡人数',
+    info: '',
+    iconClass: 'nb-audio',
+    type: 'info',
+  };
+  checkRatioCard: CardSettings = {
+    title: '打卡率',
+    info: '',
+    iconClass: 'nb-checkmark-circle',
+    type: 'warning',
+  };
+
+  statusCards: string;
+
+  commonStatusCardsSet: CardSettings[] = [
+    this.dateCard,
+    this.totalUserCard,
+    this.checkedCountCard,
+    this.checkRatioCard,
+  ];
+
+  statusCardsByThemes: {
+    default: CardSettings[];
+    cosmic: CardSettings[];
+    corporate: CardSettings[];
+  } = {
+    default: this.commonStatusCardsSet,
+    cosmic: this.commonStatusCardsSet,
+    corporate: [
+      {
+        ...this.dateCard,
+        type: 'warning',
+      },
+      {
+        ...this.totalUserCard,
+        type: 'primary',
+      },
+      {
+        ...this.checkedCountCard,
+        type: 'danger',
+      },
+      {
+        ...this.checkRatioCard,
+        type: 'secondary',
+      },
+    ],
+  };
+
+  constructor(private themeService: NbThemeService,
+              private infoService: CardService) {
+    this.themeService.getJsTheme()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe(theme => {
+        this.statusCards = this.statusCardsByThemes[theme.name];
+    });
+
+      this.infoService.getCheckDayInfoSum(this.myDate).subscribe((res) => {
+      this.cards = res;
+      this.dateCard.info = this.cards['date'];
+      this.totalUserCard.info = this.cards['totalUserCount'];
+      this.checkRatioCard.info = this.cards['checkRatio'];
+      this.checkedCountCard.info = this.cards['checkedCount'];
+      });
+
+  }
+
+
+  OnDestroy() {
+    this.alive = false;
+  }
+}
+
+
